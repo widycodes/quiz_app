0923bbc833563a4ee35b0cabe4da0abf2c72e9a6
widycodes <chonziewilliam@gmail.com>
2022-12-10 23:04:04 +0200

    column its childerm

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite color?'
+    ];
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: Text("My First App"),
-            ),
-            body: Text('This is My fist app!!!!!')));
+      appBar: AppBar(
+        title: Text("My First App"),
+      ),
+      body: Column(
+        children: [
+          Text('The question'),
+          ElevatedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          ElevatedButton(
+            child: Text('Answer 2'),
+            onPressed: () => print('Answer 2 chosen'),
+          ),
+          ElevatedButton(
+            child: Text('Answer 3'),
+            onPressed: () => print('Answer 3 chosen'),
+          ),
+        ],
+      ),
+    ));
   }
 }
