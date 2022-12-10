dcdab72d11c019817cc45f9050414caab6928eda
widycodes <chonziewilliam@gmail.com>
2022-12-10 23:13:25 +0200

    incorrect stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer chosen!');
+    questionIndex += 1;
+    print(questionIndex);
   }
 
   @override
@@ -24,7 +27,9 @@ class MyApp extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text('The question'),
+          Text(
+            questions[questionIndex],
+          ),
           ElevatedButton(
             child: Text('Answer 1'),
             onPressed: answerQuestion,
