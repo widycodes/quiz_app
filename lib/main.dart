91cea49b93def8d79c6a393f13ee5fe9d09190b7
widycodes <chonziewilliam@gmail.com>
2022-12-14 08:25:00 +0200

    basic-04: question widget created

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 // void main() {
 //   runApp(MyApp());
 // }
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void answerQuestion() {
-    questionIndex += 1;
+    setState(() {
+      questionIndex += 1;
+    });
     print(questionIndex);
   }
 
@@ -21,29 +32,30 @@ class MyApp extends StatelessWidget {
       'What\'s your favorite color?'
     ];
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("My First App"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[questionIndex],
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen'),
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () => print('Answer 3 chosen'),
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Text(
-            questions[questionIndex],
-          ),
-          ElevatedButton(
-            child: Text('Answer 1'),
-            onPressed: answerQuestion,
-          ),
-          ElevatedButton(
-            child: Text('Answer 2'),
-            onPressed: () => print('Answer 2 chosen'),
-          ),
-          ElevatedButton(
-            child: Text('Answer 3'),
-            onPressed: () => print('Answer 3 chosen'),
-          ),
-        ],
-      ),
-    ));
+    );
   }
 }
